ae03931e28781cd3df5ceef521274af9530864aa
maaario <mlipovskysk@gmail.com>
2016-08-19 11:02:34 +0200

    submit app compile translations

--- example/example/settings.py
+++ example/example/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'sk-SK'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
